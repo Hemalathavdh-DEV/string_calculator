34d9f74d9e088996a6d89e47809b296fa088f890
Hemalatha <hemalathavdh@gmail.com>
2025-05-25 15:05:31 +0530

    Different Delimiters: TDD Rule 2 -> Go Green. Added the minimal code to pass the tests

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -4,23 +4,32 @@ class StringCalculator
   def add(numbers)
     return 0 if numbers.empty?
 
-    numbers = parse_numbers(numbers)
-    sum_of_numbers_string(numbers)
+    delimiter, numbers_string = extract_delimiter_and_numbers(numbers)
+    numbers_string = parse_numbers(numbers_string, delimiter)
+    sum_of_numbers_string(numbers_string, delimiter)
   end
 
   private
 
+  def extract_delimiter_and_numbers(input)
+    return [',', input] unless input.start_with?('//')
+
+    delimiter_line, numbers_string = input.split("\n", 2)
+    delimiter = delimiter_line[2..-1] # Remove '//' prefix
+    [delimiter, numbers_string || '']
+  end
+
   # Replace newlines with commas, then split by comma
-  def parse_numbers(numbers)
-    numbers.gsub("\n", ',')
+  def parse_numbers(numbers, delimiter = ',')
+    numbers.gsub("\n", delimiter)
   end
 
   # Add the numbers in the string
-  def sum_of_numbers_string(numbers)
+  def sum_of_numbers_string(numbers, delimiter = ',')
     total = 0
     current = ''
     numbers.each_char do |char|
-      unless char == ','
+      unless char == delimiter
         current += char
         next
       end
