1ed7fd04fb4c05569c8d3c6d0de23529094b530d
Hemalatha <hemalathavdh@gmail.com>
2025-05-25 13:14:40 +0530

    Two numbers string: TDD Rule 2 -> Go Green. Added the minimal working code to pass the tests

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -4,6 +4,23 @@ class StringCalculator
   def add(numbers)
     return 0 if numbers.empty?
 
-    numbers.to_i
+    sum_of_numbers_string(numbers)
+  end
+
+  private
+
+  # Add the numbers in the string
+  def sum_of_numbers_string(numbers)
+    total = 0
+    current = ''
+    numbers.each_char do |char|
+      unless char == ','
+        current += char
+        next
+      end
+      total += current.to_i
+      current = ''
+    end
+    total + current.to_i
   end
 end
