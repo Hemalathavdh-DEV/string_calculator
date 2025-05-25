ea621cf4cf4947b9d71ae1cdd38501236558aa52
Hemalatha <hemalathavdh@gmail.com>
2025-05-25 12:48:14 +0530

    Single string: TDD Rule 1 -> Red. Added the test spec code for single string logic

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -6,9 +6,15 @@ RSpec.describe StringCalculator do
   let(:calculator) { StringCalculator.new }
 
   describe '#add' do
-    # TestCase: Empty string should return 0
+    # Empty string should return 0
     it 'returns 0 for empty string' do
       expect(calculator.add('')).to eq(0)
+    end
+
+    # Single str value should return as single integer
+    it 'returns the number itself for single number' do
+      expect(calculator.add('1')).to eq(1)
+      expect(calculator.add('5')).to eq(5)
     end
   end
 end
