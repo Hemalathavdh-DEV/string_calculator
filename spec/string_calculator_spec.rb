63da0fbf792871c94981bbe245af3324d3b705be
Hemalatha <hemalathavdh@gmail.com>
2025-05-25 22:41:04 +0530

    Additional Katas: TDD Rule 1 -> RED. Wrote spec examples for the additional testcases Long numbers, lengthy and complex delimiters

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -46,6 +46,24 @@ EDGE_CASES = {
   "//;\n1;;2" => 3
 }.freeze
 
+LARGE_NUMBER_CASES = {
+  '2,1001' => 2,
+  '1000,1' => 1001,
+  '999,1001,2' => 1001
+}.freeze
+
+LONG_DELIMITER_CASES = {
+  "//[***]\n1***2***3" => 6,
+  "//[%%]\n2%%3%%4" => 9,
+  "//[!!]\n1!!1!!1" => 3
+}.freeze
+
+COMPLEX_DELIMITER_CASES = {
+  '//[*][%]\n1*2%3' => 6,
+  '//[***][%%]\n1***2%%3' => 6,
+  '//[--][::]\n4--5::6' => 15
+}.freeze
+
 RSpec.describe StringCalculator do
   subject(:calculator) { described_class.new }
 
@@ -92,6 +110,18 @@ RSpec.describe StringCalculator do
 
     context 'edge cases' do
       include_examples 'calculates sum of the numbers string', EDGE_CASES
+    end
+
+    context 'numbers from 1001 are ignored' do
+      include_examples 'calculates sum of the numbers string', LARGE_NUMBER_CASES
+    end
+
+    context 'delimiters of any length' do
+      include_examples 'calculates sum of the numbers string', LONG_DELIMITER_CASES
+    end
+
+    context 'multiple and complex delimiters' do
+      include_examples 'calculates sum of the numbers string', COMPLEX_DELIMITER_CASES
     end
   end
 end
